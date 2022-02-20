29aea15dff58f5f9e65f61f9adc0025aa68342ff
JosePisqui201213480 <98623728+JosePisqui201213480@users.noreply.github.com>
2022-02-20 08:59:43 -0600

    Add files via upload

---
+++ CHUMPAS_201213480/CHUMPAS_201213480/Form1.vb
@@ -0,0 +1,50 @@
+﻿Public Class Form1
+
+    Private Sub Label1_Click(sender As System.Object, e As System.EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        If RadioButton1.Checked = False And RadioButton2.Checked = False And RadioButton3.Checked = False Then
+            MsgBox(" No has elegido tamaño ")
+        End If
+        If RadioButton4.Checked = False And RadioButton5.Checked = False And RadioButton6.Checked = False Then
+
+            MsgBox(" No has elegido material ")
+        End If
+
+        If TextBox1.Text = "" Then
+            MsgBox(" Ingrese una cantidad  ")
+        End If
+
+
+
+        Dim tamaño As String
+        Dim material As String
+        If RadioButton1.Checked Then
+            tamaño = "small"
+        ElseIf RadioButton2.Checked Then
+            tamaño = "medium "
+        ElseIf RadioButton3.Checked Then
+            tamaño = "large "
+
+
+        End If
+
+        If RadioButton4.Checked Then
+            tamaño = "algodon"
+        ElseIf RadioButton5.Checked Then
+            tamaño = "seda"
+        Else
+            tamaño = "lona"
+        End If
+        calcular(material, tamaño, Val(TextBox1.Text))
+        Me.Hide()
+        Form2.Show()
+
+    End Sub
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
---
+++ CHUMPAS_201213480/CHUMPAS_201213480/Module1.vb
@@ -0,0 +1,30 @@
+﻿Module Module1
+    Public Const mano_obra_small = 65.5
+    Public Const mano_obra_medium = 85.99
+    Public Const mano_obra_large = 99.99
+
+    Public Const precio_yarda_algodon = 15
+    Public Const precio_yarda_seda = 23.99
+    Public Const precio_yarda_lona = 30.99
+
+    Public Const cantida_yarda_small = 2
+    Public Const cantidad_yarda_medium = 2.5
+    Public Const cantidad_yarda_large = 3
+    Public precio_costo = 0
+    Public precio_venta = 0
+    Public Function calcular(Material As String, tamaño As String, cantidad As Double)
+        Select Case tamaño
+            Case "smal"
+                precio_costo = cantidad * cantida_yarda_small + mano_obra_small * yarda_algodon
+            Case "medium"
+                precio_costo = cantidad * cantidad_yarda_medium + mano_obra_medium
+            Case "large"
+                precio_costo = cantidad * cantidad_yarda_large + mano_obra_large
+
+                Select Case Material
+                    Case " algodon "
+                        precio_costo = cantidad * mano_obra_small + cantidad * 
+
+
+        End Select
+End Module
